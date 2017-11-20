2d731045096c7a0b032e70689e8462b671fa7500
jss <jss.nospam@gmail.com>
2017-11-20 22:52:09 +1030

    Update jss run-puppet-jss etc.

--- jss/files/run-puppet-jss.sh
+++ jss/files/run-puppet-jss.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /etc/puppetlabs/code/environments/github && git pull
-/opt/puppetlabs/bin/puppet apply github/jss/manifests/
+/opt/puppetlabs/bin/puppet apply jss/manifests/
--- jss/manifests/run-puppet-jss.pp
+++ jss/manifests/run-puppet-jss.pp
@@ -1,10 +1,10 @@
 # Set up regular Puppet runs
-file { '/usr/local/bin/run-puppet.sh':
+file { '/usr/local/bin/run-puppet-jss.sh':
   source => '/etc/puppetlabs/code/environments/github/jss/files/run-puppet-jss.sh',
   mode   => '0755',
 }
 
-cron { 'run-puppet':
+cron { 'run-puppet-jss':
   command => '/usr/local/bin/run-puppet-jss.sh',
   hour    => '*',
   minute  => '*/15',
